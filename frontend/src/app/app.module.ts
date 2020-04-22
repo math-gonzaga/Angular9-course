ab34dc348f00bf99670a8a4663c5ccd5dc0272bc
Matheus Gonzaga <matheusgonzaga10.mg@gmail.com>
2020-04-21 22:08:00 -0300

    criação do metodo criar produto, requisição ao backend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -22,7 +22,7 @@ import { ForDirective } from './directives/for.directive';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 
-
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -45,7 +45,8 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
     MatListModule,
     MatCardModule,
     MatButtonModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
