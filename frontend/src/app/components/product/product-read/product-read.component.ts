d826c156671c6a7938f80ebca5f657ee5f697663
Matheus Gonzaga <matheusgonzaga10.mg@gmail.com>
2020-04-24 00:17:48 -0300

    criação da tabela produtos

--- frontend/src/app/components/product/product-read/product-read.component.ts
+++ frontend/src/app/components/product/product-read/product-read.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Product} from './../product.model'
+import { Product } from './../product.model'
 import { ProductService } from '../product.service';
 
 
@@ -11,13 +11,13 @@ import { ProductService } from '../product.service';
 export class ProductReadComponent implements OnInit {
 
   products: Product[];
+  displayedColumns = ['id', 'name', 'price'];
 
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
-    this.productService.read().subscribe(products =>{
+    this.productService.read().subscribe(products => {
       this.products = products;
-      console.log(products);
     });
   }
 
